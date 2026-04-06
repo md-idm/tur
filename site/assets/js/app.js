5449ca42b69b38ec51c70aa185b9a6fc46013370
Alexandr Dikii <adikii@cmlatitude.com>
2026-04-06 09:47:46 +0300

    Modify contact manager form

--- site/assets/js/app.js
+++ site/assets/js/app.js
@@ -38,18 +38,6 @@ $(function () {
   });
 });
 
-document.querySelectorAll('form input[name="phone"]').forEach(function (el) {
-  window.intlTelInput(el, {
-    autoPlaceholder: "off",
-    hiddenInput: el.name,
-    initialCountry: "md",
-    preferredCountries: ["md", "ro"],
-    separateDialCode: true,
-    utilsScript:
-      "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
-  });
-});
-
 document.querySelectorAll(".dar-nav-anchor").forEach(function (el) {
   el.addEventListener("click", function () {
     const href = this.getAttribute("href");
@@ -76,85 +64,6 @@ $(".dar-cta-vacation .btn, .dar-services .btn, .dar-cta-exclusive .btn").on(
     dataLayer.push({ event: "begin_form" });
   }
 );
-
-const reCaptcha_key = "6LclghArAAAAAPnRtcjj-MEjq9D5x5PLjsy1GSnl";
-const reCaptcha = document.createElement("script");
-reCaptcha.src = "https://www.google.com/recaptcha/api.js?render=" + reCaptcha_key;
-document.head.appendChild(reCaptcha);
-
-var form_start = false;
-$("form .form-control").on("focus", function () {
-  if (!form_start) {
-    form_start = true;
-    window.dataLayer = window.dataLayer || [];
-    dataLayer.push({ event: "form_start" });
-    grecaptcha.ready(function () {
-      grecaptcha.execute(reCaptcha_key, { action: "homepage" }).then(function (token) {
-        $("form").append(
-          $("<input>", { type: "hidden", name: "g-recaptcha-response", value: token })
-        );
-      });
-    });
-  }
-});
-
-const formAlertMessages =
-  document.documentElement.lang === "ru"
-    ? {
-        recaptcha:
-          "Проверка безопасности не удалась.\nСтраница будет перезагружена, чтобы попробовать снова.",
-        fail: "Ошибка при отправке формы. Пожалуйста, попробуйте снова.",
-      }
-    : {
-        recaptcha:
-          "Verificarea de securitate nu a reușit.\nVom reîncărca pagina pentru a încerca din nou.",
-        fail: "Eroare în timpul expedierii formularului. Vă rugăm să încercați din nou.",
-      };
-
-$("form").on("submit", function (e) {
-  e.preventDefault();
-  const $btn = $(this).find('button[type="submit"]');
-  const prevText = $btn.text();
-  const waitText = $btn.data("txtexecute");
-  $btn.prop("disabled", true).text(waitText);
-  window.dataLayer = window.dataLayer || [];
-  dataLayer.push({ event: "form_submit" });
-  $.ajax({
-    url: $(this).attr("action"),
-    method: $(this).attr("method"),
-    data: $(this).serialize() + "&submit=submit",
-    dataType: "json",
-  })
-    .done(function (res) {
-      if (typeof res === "object" && res.recaptcha_error === true) {
-        alert(formAlertMessages.recaptcha);
-        location.reload();
-        return;
-      }
-      if ($("#popup").hasClass("show")) {
-        const popup = document.getElementById("popup");
-        const inst = popup ? bootstrap.Modal.getInstance(popup) : null;
-        if (inst) {
-          inst.hide();
-        }
-      }
-      new bootstrap.Modal($("#send-response")[0]).show();
-      $("#send-response .modal-body p").addClass("d-none");
-      if (res === 1 || res === true) {
-        $("#send-response .bg-success").removeClass("d-none");
-        dataLayer.push({ event: "lead_generated" });
-      } else {
-        $("#send-response .bg-danger").removeClass("d-none");
-      }
-    })
-    .fail(function () {
-      alert(formAlertMessages.fail);
-    })
-    .always(function () {
-      $btn.prop("disabled", false).text(prevText);
-    });
-  this.reset();
-});
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
